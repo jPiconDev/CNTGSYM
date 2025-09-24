efd2dddfa0415fe0aa14eda4c98278d8b4e8204f
Val <valexx55@gmail.com>
2025-09-24 17:56:55 +0200

    SeleccionContactosActivity.kt SIN PERMISOS

--- app/src/main/java/edu/cas/cntgsym/contactos/SeleccionContactosActivity.kt
+++ app/src/main/java/edu/cas/cntgsym/contactos/SeleccionContactosActivity.kt
@@ -1,7 +1,13 @@
 package edu.cas.cntgsym.contactos
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -14,6 +20,21 @@ import edu.cas.cntgsym.R
  *
  */
 class SeleccionContactosActivity : AppCompatActivity() {
+
+
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    {
+        result: ActivityResult ->
+        Log.d("MIAPP", "A la vuelta de la selección de un contacto")
+        if (result.resultCode==RESULT_OK)
+        {
+            Log.d("MIAPP", "Contacto seleccionado OK")
+            mostrarContacto(result.data!!)
+        } else {
+            Log.d("MIAPP", "Contacto seleccionado MAL/CANCELADO")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,5 +44,44 @@ class SeleccionContactosActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        selectContacto()
+
     }
+
+    fun mostrarContacto(intentDatosContacto: Intent) {
+        Log.d("MIAPP", "CONTACTO = ${intentDatosContacto.data}")
+        val cursor = contentResolver.query(intentDatosContacto.data!!, null, null, null, null)
+        if (cursor!!.moveToFirst()) //si la consulta ha devuelto resultados
+        {
+            with(cursor) //me despreocupo de cerrar - Autoclosable se cierra solo con el with
+            {
+                //cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME) // con with me ahorro además nombrar al objeto en este ámbito {}
+                val colNombre = getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+                val colNumero = getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                val nombre = getString(colNombre)
+                val numero = getString(colNumero)
+
+                Log.d("MIAPP", "NOMBRE = $nombre y NÚMERO = $numero CONTACTO SELECCIONADO")
+            }
+        }
+    }
+
+
+    fun selectContacto() {
+        Log.d("MIAPP", "URI CONTENT PROVIDER TELFS ${ContactsContract.CommonDataKinds.Phone.CONTENT_URI}")
+
+
+        val intentContactos = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+        if (intentContactos.resolveActivity(packageManager)!=null)
+        {
+            //hay una app de contactos
+            Log.d("MIAPP", "Lanzando app contactos")
+            launcher.launch(intentContactos)
+        } else {
+            Log.d("MIAPP", "Sin app de contactos")
+        }
+
+    }
+
+
 }
